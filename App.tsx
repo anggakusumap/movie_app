3368aab8735566f1455018a7db8565f900a33276
anggakusumap <kusuma_putra@student.unud.ac.id>
2024-06-27 21:27:01 +0800

    feat: finish home screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
+// App.tsx
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 import BottomTabNavigator from './src/navigations/BottomTabNavigation'
+import MovieDetail from './src/screens/MovieDetail'
+
+const Stack = createStackNavigator()
 
 export default function App(): JSX.Element {
   return (
     <NavigationContainer>
-      <BottomTabNavigator />
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Main"
+          component={BottomTabNavigator}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="MovieDetail"
+          options={{ headerShown: false }}
+          component={MovieDetail}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   )
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,56 @@
 import React from 'react'
-import { Text } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { ScrollView, View, StatusBar, StyleSheet } from 'react-native'
+import MovieList from '../components/movies/MovieList'
+import type { MovieListProps } from '../types/app'
 
-export default function Home(): JSX.Element {
+const movieLists: MovieListProps[] = [
+    {
+        title: 'Now Playing in Theater',
+        path: 'movie/now_playing?language=en-US&page=1',
+        coverType: 'backdrop',
+    },
+    {
+        title: 'Upcoming Movies',
+        path: 'movie/upcoming?language=en-US&page=1',
+        coverType: 'poster',
+    },
+    {
+        title: 'Top Rated Movies',
+        path: 'movie/top_rated?language=en-US&page=1',
+        coverType: 'poster',
+    },
+    {
+        title: 'Popular Movies',
+        path: 'movie/popular?language=en-US&page=1',
+        coverType: 'poster',
+    },
+]
+
+const Home = (): JSX.Element => {
     return (
-        <SafeAreaView>
-            <Text>Home</Text>
-        </SafeAreaView>
+        <ScrollView>
+            <View style={styles.container}>
+                {movieLists.map((movieList) => (
+                    <MovieList
+                        title={movieList.title}
+                        path={movieList.path}
+                        coverType={movieList.coverType}
+                        key={movieList.title}
+                    />
+                ))}
+                <StatusBar translucent={false} />
+            </View>
+        </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: StatusBar.currentHeight ?? 32,
+        alignItems: 'center',
+        justifyContent: 'center',
+        rowGap: 16,
+    },
+})
+
+export default Home
